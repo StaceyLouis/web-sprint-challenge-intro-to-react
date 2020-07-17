415ebcd520362ffaab923e60a1b7747edadc3dc3
Stacey Louis <stacey-louis@lambdastudents.com>
2020-07-17 13:15:48 -0400

    mvp

---
+++ starwars/src/components/CharContainer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Character from './Character';
+import styled from 'styled-components';
+import "../index.css"
+
+const ParentDiv = styled.div`
+ 
+ display: flex;
+  flex-flow: row wrap;
+  ;
+`
+
+
+function CharContainer(props) {
+
+    const { data } = props;
+    console.log(data)
+    return (
+        <ParentDiv>
+      
+        {data.map((i) => {
+      
+        return   <Character key={i.id} name={i.name} status={i.status} species={i.species} image={i.image} />;
+      })};
+
+
+        
+        </ParentDiv>
+    )
+}
+
+export default CharContainer;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,37 @@
 // Write your Character component here
+import React from "react";
+import styled from 'styled-components';
+import "../index.css"
+
+const CharacterDiv = styled.div `
+ display: flex;
+flex-flow: row;
+justify-content: space between;
+color: #99adc1;
+margin: 2%;
+
+`
+
+
+
+function Character(props){
+    console.log(props);
+    return(
+        <CharacterDiv>
+            
+            <div className="cardWrap" style={{flex: 1, flexDirection: 'row'}}>
+             
+    <img id="charImage" src={props.image} alt={props.name}  />
+             
+    <p className="user"> {props.name}</p>
+
+    <p className="comment">Species: {props.species}</p>
+    <p> Status: {props.status}</p>
+    </div>
+ 
+        
+        </CharacterDiv>
+    )
+}
+
+export default Character;
